09ca638a77938c2c483384a2a06b598e53b15d82
savelgmx <savel@gmx.com>
2021-12-12 17:29:18 +0700

    Links in main file

--- app/src/main/java/com/example/forecastmvvm/MainActivity.kt
+++ app/src/main/java/com/example/forecastmvvm/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.forecastmvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//https://github.com/ResoCoder/forecast-mvvm-android-kotlin
+//https://www.youtube.com/watch?v=yDaaM3u389I
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
