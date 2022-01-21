c3f487b7b9bba6da3df595fd480a85aed14b49bb
savelgmx <savel@gmx.com>
2022-01-21 15:51:10 +0700

    #6 ViewModel ViewModelFactory TODO устанить Cannot create an instance of class ViewModel

--- app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherFragment.kt
@@ -1,5 +1,6 @@
 package com.example.forecastmvvm.ui.weather.current
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,10 +8,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.forecastmvvm.R
+import com.example.forecastmvvm.data.db.unitlocalized.current.UnitSpecificCurrentWeatherEntry
 import com.example.forecastmvvm.data.network.ConnectivityInterceptorImpl
 import com.example.forecastmvvm.data.network.WeatherNetworkDataSourceImpl
 import com.example.forecastmvvm.data.network.WeatherstackApiService
+import com.example.forecastmvvm.ui.base.ScopedFragment
 import kotlinx.android.synthetic.main.current_weather_fragment.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -20,7 +24,7 @@ import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
 
 
-class CurrentWeatherFragment() : Fragment(),KodeinAware {
+class CurrentWeatherFragment() : ScopedFragment(),KodeinAware {
 
     override val kodein by closestKodein()
     private val viewModelFactory: CurrentWeatherViewModelFactory by instance()
@@ -40,20 +44,26 @@ class CurrentWeatherFragment() : Fragment(),KodeinAware {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
-        // TODO: Use the ViewModel
 
-        val apiService = WeatherstackApiService(ConnectivityInterceptorImpl(this.context!!))
-        val weatherNetworkDataSource = WeatherNetworkDataSourceImpl(apiService)
-        weatherNetworkDataSource.downloadedCurrentWeather.observe(this,
-            Observer {
-                textView.text= it.toString()
+      //  viewModel = ViewModelProviders.of(this, viewModelFactory)
+        //    .get(CurrentWeatherViewModel::class.java)
+
+        viewModel = ViewModelProvider(this).get(CurrentWeatherViewModel::class.java)
+
+        bindUI()
+    }
+
+
+    @SuppressLint("FragmentLiveDataObserve")
+    private fun bindUI()=launch{
+
+        val currentWeather = viewModel.weather.await()
+
+        currentWeather.observe(this@CurrentWeatherFragment, Observer {
+            if (it==null) return@Observer
+            textView.text = it.toString()
         })
 
-        GlobalScope.launch(Dispatchers.Main) {
-            weatherNetworkDataSource.fetchCurrentWeather("Krasnoyarsk","en")
-
-        }
     }
 
 }
