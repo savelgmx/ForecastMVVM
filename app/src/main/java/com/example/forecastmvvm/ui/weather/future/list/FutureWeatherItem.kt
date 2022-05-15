5fd28fb86587c220e957d158ece33a9b98e51c10
savelgmx <savel@gmx.com>
2022-05-15 18:02:18 +0700

    Recycler View  item class rewrited to show any items //TODO use ViewModel in FutureListWeatherFragment

--- app/src/main/java/com/example/forecastmvvm/ui/weather/future/list/FutureWeatherItem.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/future/list/FutureWeatherItem.kt
@@ -1,10 +1,16 @@
 package com.example.forecastmvvm.ui.weather.future.list
 
+import android.util.Log
 import com.example.forecastmvvm.R
 import com.example.forecastmvvm.data.network.response.forecast.FutureWeatherResponse
+import com.resocoder.forecastmvvm.internal.glide.GlideApp
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.current_weather_fragment.*
 import kotlinx.android.synthetic.main.item_future_weather.*
+import kotlinx.android.synthetic.main.item_future_weather.imageView_condition_icon
+import kotlinx.android.synthetic.main.item_future_weather.textView_condition
+import kotlinx.android.synthetic.main.item_future_weather.textView_temperature
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.format.FormatStyle
 
@@ -14,31 +20,45 @@ class FutureWeatherItem(
 ) : Item() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.apply {
-           // textView_condition.text = weatherEntry.conditionText
-            updateDate()
-            updateTemperature()
-          //  updateConditionImage()
+
+            textView_condition.text = weatherEntry.daily[1].dt.toString()
+
+            val range = weatherEntry.daily.size
+
+            for (i in 0 until range) {
+                Log.d("t-WeatherEntry",weatherEntry.daily[i].temp.day.toString())
+                Log.d("d-WeatherEntry",weatherEntry.daily[i].dt.toString())
+                Log.d("c-WeatherEntry",weatherEntry.daily[i].toString())
+
+                updateDate(weatherEntry.daily[i].dt.toString())
+                updateTemperature(weatherEntry.daily[i].temp.day.toString())
+                updateConditionImage(weatherEntry.daily[i].weather?.get(0)?.icon)
+
+            }
         }
     }
 
     override fun getLayout() = R.layout.item_future_weather
 
-    private fun ViewHolder.updateDate() {
+    private fun ViewHolder.updateDate(toString: String) {
         val dtFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)
-     //   textView_date.text = weatherEntry.date.format(dtFormatter)
+        textView_date.text = toString.format(dtFormatter)
     }
 
-    private fun ViewHolder.updateTemperature() {
+    private fun ViewHolder.updateTemperature(temp:String) {
         val unitAbbreviation = "°C" /*if (weatherEntry is MetricSimpleFutureWeatherEntry) "°C"
         else "°F"*/
-        textView_temperature.text ="" // "${weatherEntry.avgTemperature}$unitAbbreviation"
+        textView_temperature.text =  "${temp}$unitAbbreviation"
     }
 
-/*
-    private fun ViewHolder.updateConditionImage() {
+    private fun ViewHolder.updateConditionImage(icon: String) {
+        val iconurl = "http://openweathermap.org/img/w/"
         GlideApp.with(this.containerView)
-            .load("http:" + weatherEntry.conditionIconUrl)
+            .load(
+                "$iconurl${icon}"+".png"
+            )
             .into(imageView_condition_icon)
+
+
     }
-*/
 }
