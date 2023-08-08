de910b17530e88e6fe5b8da5a917a0bc21a58672
a <savel@gmx.com>
2023-08-08 16:28:40 +0700

    rewrite CurrentWeatherViewModel to avoid lazy deffered jet not tested //TODO make show binduI AS EXPECTED

--- app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
@@ -1,32 +1,31 @@
 package com.example.forecastmvvm.ui.weather.current
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import com.example.forecastmvvm.data.db.entity.CurrentWeatherEntry
 import com.example.forecastmvvm.data.repository.ForecastRepository
 import com.example.forecastmvvm.internal.UnitSystem
-import com.example.forecastmvvm.internal.lazyDeffered
-import com.example.forecastmvvm.ui.base.WeatherViewModel
 import com.resocoder.forecastmvvm.data.provider.UnitProvider
+import kotlinx.coroutines.launch
+
 
 class CurrentWeatherViewModel (
     private val forecastRepository: ForecastRepository,
     unitProvider: UnitProvider
-):  WeatherViewModel(forecastRepository, unitProvider ) {
+) : ViewModel() {
     private val unitSystem = unitProvider.getUnitSystem()
 
-    val isMetric:Boolean
-        get() = unitSystem==UnitSystem.METRIC
-/*
-    we have to call getCurrentweather from within lazy
-    block as is getCurrentWeather a suspend function
-    within lazy block it calls only when necessary
-    to provide coroutine context we also have write lazyDeffered function
+    private val isMetric: Boolean = unitProvider.getUnitSystem() == UnitSystem.METRIC
 
-*/
+    // Using MutableLiveData to hold the weather data
+    private val _weather = MutableLiveData<CurrentWeatherEntry>()
 
-    val weather by lazyDeffered(viewModelScope) {
-        forecastRepository.getCurrentWeather(isMetric)
+    // Exposing LiveData to be observed
+    val weather: LiveData<CurrentWeatherEntry>
+        get() = _weather
+
+    // Fetching weather data and updating _weather
+    suspend fun fetchWeather() {
+        val currentWeather = forecastRepository.getCurrentWeather(isMetric)
+        _weather.postValue(currentWeather.value) // Use postValue() to update LiveData from background thread
     }
-
 }
-
