6e07d34beb3c2f0c39791d1d4a78649abaccf0b9
savelgmx <savel@gmx.com>
2023-08-30 19:38:48 +0700

    Added DetailedViewFragment date from list in FutureListWeatherFragment //TODO change data type from sting to Daily object and populate view

--- app/src/main/java/com/example/forecastmvvm/ui/weather/future/detail/FutureDetailWeatherFragment.kt
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/future/detail/FutureDetailWeatherFragment.kt
@@ -2,10 +2,12 @@ package com.example.forecastmvvm.ui.weather.future.detail
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import com.example.forecastmvvm.R
 import com.example.forecastmvvm.data.network.response.forecast.Daily
 import com.example.forecastmvvm.ui.base.ScopedFragment
@@ -31,6 +33,15 @@ class FutureDetailWeatherFragment : ScopedFragment(), KodeinAware {
     ): View? {
         return inflater.inflate(R.layout.future_detail_weather_fragment, container, false)
         }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val safeArgs = arguments?.let { FutureDetailWeatherFragmentArgs.fromBundle(it)
+
+        Log.d("FutureWeatherDetail","FutureWeatherDetail"+it.toString())
+        }
+
+    }
 
     private fun populateUI(selectedDay: Daily) {
         // Populate your UI elements with data from selectedDay
